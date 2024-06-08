6513d418de3403d0dbf61e984b7f90b3063ff983
Rui Rua <rui.rrua@gmail.com>
2024-06-08 23:46:51 +0100

    search bar on app folder

---
+++ README.md
@@ -0,0 +1,6 @@
+# Terminal CV
+
+## Known-Issues / TODO
+- terminal content isn't scrollable
+- auto-complete on terminal breaks when cmd arg > 1
+- a window close closes more than 1 window when there are many windows and the window closed is not the last
--- src/view/AppFolder.js
+++ src/view/AppFolder.js
@@ -13,6 +13,26 @@ class ViewType {
     }
   }
 
+const Listbox = ({ options, onSelect, default_text="Select attribute"}) => {
+    const [selectedOption, setSelectedOption] = useState('');
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setSelectedOption(value);
+        onSelect(value);
+    };
+
+    return (
+        <select className='top_bar_select' value={selectedOption} onChange={handleChange}>
+            <option value="">{default_text}</option>
+            {options.map((option, index) => (
+                <option key={index} value={option}>
+                {option}
+                </option>
+            ))}
+        </select>
+    );
+};
+
 const FolderEntry = ({key, icon, name, onclick}) => {
     const [showTooltip, setShowTooltip] = useState(false);
     return  (
@@ -30,10 +50,53 @@ const FolderEntry = ({key, icon, name, onclick}) => {
 };
 
 
-const AppFolder = ({name, openApp, data, view_type=ViewType.List}) => {
+const AppFolder = ({name, openApp, data, view_type=ViewType.List, searchBar=true}) => {
+    const [dataToShow, setDataToShow] = useState(data);
+    const [inputValue, setInputValue] = useState('');
+    const [selectedOption, setSelectedOption] = useState('');
+    const default_text = "Select attribute";
+
+    const getOptionsOnData = (data) => {
+        let keys = new Set();
+        data.forEach((file) => {
+            Object.keys(file).forEach((key) => keys.add(key));
+        });
+        return Array.from(keys);
+    };
+
+    const handleSelect = (value) => {
+        setSelectedOption(value);
+        handleInputChange({target: {value: inputValue}});
+      };
+
+    const handleInputChange = (value) => {
+        setInputValue(value.target.value);
+        if (value.target.value === '') {
+            setDataToShow(data);
+            return;
+        }
+        const filteredData = data.filter((file) => {
+            return Object.keys(file).some((key) => {
+                const search_val = (selectedOption === ''|| selectedOption === default_text || !file[selectedOption]) ? key : selectedOption;
+                return typeof file === 'object' ? 
+                        file[search_val].toString().toLowerCase().includes(value.target.value.toLowerCase()) 
+                            : (Array.isArray(file) ? file[search_val].join(", ").toLowerCase().includes(value.target.value.toLowerCase()) 
+                                : false);
+            });
+        });
+        setDataToShow(filteredData);
+    }
+
     return (
         <div className="folder_content">
-            {data.map((file, index) => (
+           {searchBar && 
+                <div className="folder_top_bar">
+                    <Listbox options={getOptionsOnData(data)} onSelect={handleSelect} default_text={default_text} />
+                    <input value={inputValue} type="text" placeholder="Search..." onChange={handleInputChange} />
+                    {/*<button><img className='top_bar_img' src={require("../assets/search-icon.png")}></img></button>*/}
+                </div>  
+            }
+            {dataToShow.map((file, index) => (
                 view_type === ViewType.Grid ?
                     <DesktopIcon  key={index} 
                     icon={{
